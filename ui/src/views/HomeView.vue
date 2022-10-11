b431722b8d7593c0ca8f0137aa0761430edf06e7
testandwin <mschlottmann74@gmail.com>
2022-10-11 18:21:39 +0200

    Update doc

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">e2e test coverage</h5>
-        <p class="card-text">e2e test coverage gives an overview of the different product areas and how many automated tests exist in each area.</p>
+        <p class="card-text">e2e test coverage gives an overview of the different product areas and how many automated tests exist in each area with their respective test status.</p>
         <p>Additionally, the impressions of exploratory tests on the different product areas can be gathered.</p>
         <p>In the test coverage views, the test results of the last 28 days are taken into account.</p>
         <p class="card-text"><a href="/docs/index.html" class="link-info">API Documentation</a></p>
